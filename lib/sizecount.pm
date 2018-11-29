edbb65026e4678d5fd40c0f75a798708e64b69e9
Jason Hustedt <jhustedt@cbcbsub01.umiacs.umd.edu>
2018-11-29 14:54:20 -0500

    Adding initial files & directories

---
+++ lib/sizecount.pm
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+package sizecount;
+
+## LICENSE: gplv3
+## ABSTRACT: A small script for counting DNA sequencing fragments of specific sizes based on their sequence.
+
+1;
