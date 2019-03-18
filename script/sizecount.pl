a92f2f90fb76fa0db851ca56621a8ce2402e9ba1
Jason Hustedt <jasonhustedt@astraeus4.net>
2019-03-18 19:59:38 -0400

    WIP

--- script/sizecount.pl
+++ script/sizecount.pl
@@ -505,13 +505,13 @@ sub Sort_File_Approx {
                 } else {
                     $unicyclized4_full_final_lengths{$numbers{stepsynth_fwd}.$numbers{variable_fwd}.$numbers{helical_fwd}}++;
                 }
-                my $short_size = ($numbers{stepsynth_fwd} + $numbers{variable_fwd} + $numbers{helical_fwd} + $options{spacer});
+                my $short_size = [$numbers{stepsynth_fwd} + $numbers{variable_fwd} + $numbers{helical_fwd} + $options{spacer}];
                         foreach my $shortsize (@{$short_size}) {
                     if (!defined($unicyclized4_final_lengths{$shortsize})) {
                         $unicyclized4_final_lengths{$shortsize} = 0;
                     }
                 }
-                my $full_size = ($numbers{stepsynth_fwd}.$numbers{variable_fwd}.$numbers{helical_fwd});
+                my $full_size = [$numbers{stepsynth_fwd}.$numbers{variable_fwd}.$numbers{helical_fwd}];
                         foreach my $fullsize (@{$full_size}) {
                     if (!defined($unicyclized4_full_final_lengths{$fullsize})) {
                         $unicyclized4_full_final_lengths{$fullsize} = 0;
